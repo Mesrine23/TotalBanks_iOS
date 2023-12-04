a0066c54a49ed6b3083d3b0546fc9ab3dc6d845c
Panagiotis Palamidas <palas.pan.23@gmail.com>
2023-12-04 18:29:05 +0200

    Project Init

---
+++ TotalBanks_iOS/TotalBanks_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TotalBanks_iOS
+//
+//  Created by Panagiotis Palamidas on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TotalBanks_iOS/TotalBanks_iOS/TotalBanks_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalBanks_iOSApp.swift
+//  TotalBanks_iOS
+//
+//  Created by Panagiotis Palamidas on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TotalBanks_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
